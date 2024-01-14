c7ea9910c486e4fc8d6f4d719ed3d71f9e3cd075
Sybren Dijkstra <nerbiz@gmail.com>
2024-01-14 17:43:33 +0100

    Better class usage in tests

--- tests/Feature/AccessTest.php
+++ tests/Feature/AccessTest.php
@@ -1,12 +1,14 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Nerbiz\CrawlShield\Middleware\CrawlShieldMiddleware;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 it('allows access with correct password', function () {
     $request = Request::create('/?pass=abc123');
     $middleware = new CrawlShieldMiddleware();
-    $response = $middleware->handle($request, fn () => response('Ok', 200));
+    $response = $middleware->handle($request, fn () => new Response('Ok', 200));
 
     expect($response->getStatusCode())->toBe(200);
 });
@@ -14,11 +16,11 @@ it('allows access with correct password', function () {
 it('denies access with incorrect password', function () {
     $request = Request::create('/?pass=wrong');
     $middleware = new CrawlShieldMiddleware();
-    $middleware->handle($request, fn () => response('Ok', 200));
-})->throws(\Symfony\Component\HttpKernel\Exception\HttpException::class);
+    $middleware->handle($request, fn () => new Response('Ok', 200));
+})->throws(HttpException::class);
 
 it('denies access without a password', function () {
     $request = Request::create('/');
     $middleware = new CrawlShieldMiddleware();
-    $middleware->handle($request, fn () => response('Ok', 200));
-})->throws(\Symfony\Component\HttpKernel\Exception\HttpException::class);
+    $middleware->handle($request, fn () => new Response('Ok', 200));
+})->throws(HttpException::class);
